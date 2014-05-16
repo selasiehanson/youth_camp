26b36fe8beaab82c60233bd9480bab15ca988ba3
selasiehanson <soundfever18@gmail.com>
2014-05-16 00:47:21 +0000

    color changes

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -7,8 +7,21 @@ class RegistrationController < ApplicationController
   def show   
   end
 
+   def new    
+    setup_view
+    @camper = CamperFormObject.new({email: "", phone_number: ""})
+  end
+
+
   def create
     camper = CamperFormObject.to_camper(CamperFormObject.new(camper_params.to_h))
+    status = camper_exists(camper)
+    if status[0] == true
+      flash[:danger] = status[1]
+      setup_view
+      @camper = CamperFormObject.from_camper(camper)
+      redirect_to registration_new_url and return
+    end
     if camper.save
       sign_in camper
       redirect_to camper
@@ -20,14 +33,7 @@ class RegistrationController < ApplicationController
     end
   end
 
-  def new
-    email = params[:email]
-    phone_number = params[:phone_number]
-    setup_view
-    @camper = CamperFormObject.new({email: email, phone_number: phone_number})
-    p @camper
-  end
-
+ 
   def setup_view
     @countries = Country::LIST.collect {|p| [ p[:printable_name], p[:name] ] }
     @schools = School::LIST.collect {|p| [ p[:name], p[:name] ] }
@@ -49,33 +55,54 @@ class RegistrationController < ApplicationController
   def update
   end
 
-  def sign_up
-    if params[:camper][:phone_number].blank? || params[:camper][:email].blank?
-      # flash message
-      flash[:danger] = 'Both email and phone number are required'
-      redirect_to root_url and return
-    end
-    phone_number = params[:camper][:phone_number]
-    email = params[:camper][:email]
-    # camper =
+  # def sign_up
+  #   if params[:camper][:phone_number].blank? || params[:camper][:email].blank?
+  #     # flash message
+  #     flash[:danger] = 'Both email and phone number are required'
+  #     redirect_to root_url and return 
+  #   end
+  #   phone_number = params[:camper][:phone_number]
+  #   email = params[:camper][:email]
+  #   # camper =
+  #   if Camper.find_by(email: email, phone_number: phone_number)
+  #     # add errors
+  #     flash[:danger] = 'A camper with the same credential already exists.
+  #     If you are the one kindly login instead'
+  #     redirect_to root_url and return
+  #   elsif Camper.find_by(email: email)
+  #     # add errors
+  #     flash[:danger] = 'A camper with the same EMAIL already exists.
+  #     If you are the one kindly login instead'
+  #     redirect_to root_url and return
+  #   elsif Camper.find_by(phone_number: phone_number)
+  #     # add errors
+  #     flash[:danger] = 'A camper with the same PHONE NUMBER already exists.
+  #     If you are the one kindly login instead'
+  #     redirect_to root_url and return
+  #   else
+  #     redirect_to "/registration/new?email=#{email}&phone_number=#{phone_number}"
+  #   end
+  # end
+
+  def camper_exists(camper)
+    email = camper.email
+    phone_number = camper.phone_number
+    status = [false, ""]
+
     if Camper.find_by(email: email, phone_number: phone_number)
-      # add errors
-      flash[:danger] = 'A camper with the same credential already exists.
-      If you are the one kindly login instead'
-      redirect_to root_url and return
+      message = 'A camper with the same credential already exists.
+      If you are the one kindly login instead'      
+      status=[true, message]
     elsif Camper.find_by(email: email)
-      # add errors
-      flash[:danger] = 'A camper with the same EMAIL already exists.
+      message = 'A camper with the same EMAIL already exists.
       If you are the one kindly login instead'
-      redirect_to root_url and return
-    elsif Camper.find_by(phone_number: phone_number)
-      # add errors
-      flash[:danger] = 'A camper with the same PHONE NUMBER already exists.
+      status=[true, message]
+    elsif Camper.find_by(phone_number: phone_number)      
+      message = 'A camper with the same PHONE NUMBER already exists.
       If you are the one kindly login instead'
-      redirect_to root_url and return
-    else
-      redirect_to "/registration/new?email=#{email}&phone_number=#{phone_number}"
+      status=[true, message]
     end
+    status
   end
 
   def camper_params
