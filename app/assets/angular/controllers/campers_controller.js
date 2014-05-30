4c680307ca5280a0be561a4de2721a5e8e033b21
selasiehanson <soundfever18@gmail.com>
2014-05-30 13:47:46 +0000

    added refresh button to camper's list

--- app/assets/angular/controllers/campers_controller.js
+++ app/assets/angular/controllers/campers_controller.js
@@ -1,21 +1,32 @@
 var app = angular.module("app.controllers");
 
 app.controller("CampersController", ['$scope', 'Camper',
-    function($scope, Camper) {
-      $scope.campers = {}
-      window.CP = Camper
+  function($scope, Camper) {
+    $scope.isFetching = false;
+    $scope.campers = {}
+    window.CP = Camper
+
+
+    $scope.fetchData = function() {
+      $scope.isFetching = true;
       Camper.query().$promise.then(function(res) {
-          Camper.data = $scope.campers = res;
-      });
-      }
-    ]);
-
-
-  app.controller("CampersShowController", ['$scope', 'Camper', '$stateParams',
-    function($scope, Camper, $stateParams) {
-      
-      Camper.get({id : $stateParams.id}).$promise.then(function (res){
-        $scope.camper = res
+        Camper.data = $scope.campers = res;
+        $scope.isFetching = false;
       });
     }
-  ]);
+
+    $scope.fetchData();
+  }
+]);
+
+
+app.controller("CampersShowController", ['$scope', 'Camper', '$stateParams',
+  function($scope, Camper, $stateParams) {
+
+    Camper.get({
+      id: $stateParams.id
+    }).$promise.then(function(res) {
+      $scope.camper = res
+    });
+  }
+]);
