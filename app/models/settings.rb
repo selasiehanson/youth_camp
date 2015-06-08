6b59cf31dd21af304c02e968d557c96c28dafc78
selasiehanson <soundfever18@gmail.com>
2015-06-08 00:35:46 +0000

    changed closing date for registeration

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,5 +1,5 @@
 module Settings
-  REGISTRATION_END_DATE = "2015-06-06"
+  REGISTRATION_END_DATE = "2015-06-17"
 
   def Settings.registration_ended?
     Date.today > Date.parse(REGISTRATION_END_DATE)
