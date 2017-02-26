aaa59f709e72f95887d1ae95b582604dcac62b00
lazaraus <joe@lazaraus>
2017-02-26 08:19:11 -0500

    fixed lookup.rb

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -1,52 +1,52 @@
 class Lookup
   SCHOOL_LOCATIONS = [
-    { name: "Ashanti Region (Kumasi)" },
-    { name: "Brong-Ahafo Region (Sunyani)" },
-    { name: "Central Region (Cape Coast)" },
-    { name: "Eastern Region (Koforidua)" },
-    { name: "Greater Accra Region (Accra)" },
-    { name: "Northern Region (Tamale)" },
-    { name: "Upper East Region (Bolgatanga)" },
-    { name: "Upper West Region (Wa)" },
-    { name: "Volta Region (Ho)" },
-    { name: "Western Region (Sekondi-Takoradi)" },
-    { name: "other" }
+    { "name": "Ashanti Region (Kumasi)" },
+    { "name": "Brong-Ahafo Region (Sunyani)" },
+    { "name": "Central Region (Cape Coast)" },
+    { "name": "Eastern Region (Koforidua)" },
+    { "name": "Greater Accra Region (Accra)" },
+    { "name": "Northern Region (Tamale)" },
+    { "name": "Upper East Region (Bolgatanga)" },
+    { "name": "Upper West Region (Wa)" },
+    { "name": "Volta Region (Ho)" },
+    { "name": "Western Region (Sekondi-Takoradi)" },
+    { "name": "other" }
   ]
 
   EDUCATIONAL_LEVELS = [
-     { name: "jhs student" },
-     { name: "shs" },
-     { name: "tertiary" }
+     { "name": "jhs student" },
+     { "name": "shs" },
+     { "name": "tertiary" }
    ]
 
   ROLES = [
-     { name: "camper" },
-     { name: "committee" },  
-     { name: "prayer team " },
-     { name: "worship/tech team" },
+     { "name": "camper" },
+     { "name": "committee" },  
+     { "name": "prayer team " },
+     { "name": "worship/tech team" },
   ]
 
    ARRIVAL_DAYS = [
-     { name:"monday" },
-     { name:"tuesday" }
-     { name: "wednesday" },
-     { name: "thursday" },
-     { name: "friday" },
-     { name: "saturday" },
-     { name: "sunday" }
+     { "name":"monday" },
+     { "name":"tuesday" },
+     { "name": "wednesday" },
+     { "name": "thursday" },
+     { "name": "friday" },
+     { "name": "saturday" },
+     { "name": "sunday" }
    ]
 
    ARRIVAL_TIMES = [
-     { name: "morning" },
-     { name: "afternoon" },
-     { name: "evening" }
+     { "name": "morning" },
+     { "name": "afternoon" },
+     { "name": "evening" }
    ]
 
    CHURCHES = [
-      { name: "Ridge" },
-      { name: "Manet" },
-      { name: "Tudu" },
-      { name: "Other" }
+      { "name": "Ridge" },
+      { "name": "Manet" },
+      { "name": "Tudu" },
+      { "name": "Other" }
    ]
 
 end
