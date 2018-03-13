d84fbada9bbd7ed45aa4dfbf651cb7d9c24595ff
lazaraus <joe@lazaraus>
2018-03-13 11:48:07 -0400

    opened registration for 2018

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,5 +1,5 @@
 module Settings
-  REGISTRATION_END_DATE = "2017-06-07"
+  REGISTRATION_END_DATE = "2018-06-07"
 
   def Settings.registration_ended?
     Date.today > Date.parse(REGISTRATION_END_DATE)
