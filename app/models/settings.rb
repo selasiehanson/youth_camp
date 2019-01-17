e3727bdceb354ba66c04ada48877ec6af11e358e
lazaraus <joe@lazaraus>
2019-01-17 14:35:06 -0500

    update registration close date to 2019-6-15

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,6 +1,6 @@
 module Settings
 
-  REGISTRATION_END_DATE = "2018-06-10" 
+  REGISTRATION_END_DATE = "2019-06-18" 
  def Settings.registration_ended?
     Date.today > Date.parse(REGISTRATION_END_DATE)
   end
